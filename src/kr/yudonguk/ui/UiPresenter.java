5f9097bff53f1f719280d72804caf309f83fed4e
yudonguk <yudonguk@naver.com>
2013-02-19 19:09:01 +0900

    UiPresenter 인터페이스의 getData 메소드 제거
    1. 동기식으로 데이터를 가져오는 Data getData(int id) 메소드를 제거, 이후 UiView 쪽에서 데이터를 가져오기
    위해서는 반드시 비동기식 으로 데이터를 가져오는 void getData(int id, DataReceiver<Data>
    receiver)를 이용해야함

--- src/kr/yudonguk/ui/UiPresenter.java
+++ src/kr/yudonguk/ui/UiPresenter.java
@@ -1,12 +1,9 @@
 package kr.yudonguk.ui;
 
-
 public interface UiPresenter<Data>
 {
 	void onModelUpdated(UpdateResult result);
 
-	Data getData(int id);
-	
 	void getData(int id, DataReceiver<Data> receiver);
 
 	void setData(int id, Data data);
