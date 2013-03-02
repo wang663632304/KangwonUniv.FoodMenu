63bbcc6a6bbeb8c60e994476d9d3518f0aa9cc13
yudonguk <yudonguk@naver.com>
2013-03-03 03:11:23 +0900

    KnuCoopFoodMenuModel 수정
    1. getData() 메소드에 입력된 일 수(id)가 KnuCoopFoodMenuParser에서 가져온 WeekFoodMenu에
    포함된 날이 아닐경우 null 을 반환 하도록 수정
    2. getWeekFoodMenu() 메소드 생성

--- src/kr/yudonguk/kangwonuniv/foodmenu/data/KnuCoopFoodMenuModel.java
+++ src/kr/yudonguk/kangwonuniv/foodmenu/data/KnuCoopFoodMenuModel.java
@@ -4,53 +4,73 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Calendar;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 import kr.yudonguk.kangwonuniv.foodmenu.data.WeekFoodMenu.Week;
 import kr.yudonguk.kangwonuniv.foodmenu.data.parser.FoodMenuParser;
 import kr.yudonguk.kangwonuniv.foodmenu.data.parser.KnuCoopFoodMenuParser;
+import kr.yudonguk.util.calendar.CalendarUtil;
 
 public abstract class KnuCoopFoodMenuModel implements FoodMenuModel
 {
 	private static final FoodMenuParser parser = new KnuCoopFoodMenuParser();
-	private WeekFoodMenu weekFoodMenu = null;
-	private final Lock mWeekFoodMenuLock = new ReentrantLock();
+	private WeekFoodMenu mWeekFoodMenu = null;
+	private final Calendar mWeekFoodMenuCalendar = Calendar.getInstance();
 
 	@Override
 	public FoodMenu getData(int id)
 	{
-		if (weekFoodMenu == null)
+		if (mWeekFoodMenu == null)
 		{
-			synchronized (mWeekFoodMenuLock)
+			synchronized (mWeekFoodMenuCalendar)
 			{
-				if (weekFoodMenu == null)
+				if (mWeekFoodMenu == null)
 				{
-					try
-					{
-						URL url = new URL(getKnuCoopUrl());
-						weekFoodMenu = parser.parse(url);
-					} catch (MalformedURLException e)
-					{
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IOException e)
-					{
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+					mWeekFoodMenuCalendar.clear();
+					mWeekFoodMenuCalendar.add(Calendar.DAY_OF_MONTH, id);
+					mWeekFoodMenu = getWeekFoodMenu(mWeekFoodMenuCalendar);
 
-					if (weekFoodMenu == null)
+					if (mWeekFoodMenu == null)
 						return null;
 				}
 			}
 		}
 
+		Calendar startDate = Calendar.getInstance();
+		Calendar endDate = Calendar.getInstance();
+		CalendarUtil.getWeekBoundary(startDate, endDate, mWeekFoodMenuCalendar,
+				Calendar.MONDAY);
+
 		Calendar calendar = Calendar.getInstance();
 		calendar.clear();
 		calendar.add(Calendar.DAY_OF_MONTH, id);
+
+		if (calendar.compareTo(startDate) < 0
+				|| calendar.compareTo(endDate) > 0)
+			return null;
+
 		Week dayOfWeek = Week.toWeek(calendar.get(Calendar.DAY_OF_WEEK));
-		return weekFoodMenu.get(dayOfWeek);
+		return mWeekFoodMenu.get(dayOfWeek);
+	}
+
+	public WeekFoodMenu getWeekFoodMenu(Calendar calendar)
+	{
+		WeekFoodMenu weekFoodMenu = null;
+		try
+		{
+			URL url = new URL(getKnuCoopUrl());
+			weekFoodMenu = parser.parse(url);
+		} catch (MalformedURLException e)
+		{
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+
+		if (weekFoodMenu == null)
+			return null;
+
+		return weekFoodMenu;
 	}
 
 	public abstract String getKnuCoopUrl();
