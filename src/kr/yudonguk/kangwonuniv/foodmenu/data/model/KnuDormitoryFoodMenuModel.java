c3ffc6164f7e802f17f9778119e8dc895f1e8fa8
yudonguk <yudonguk@naver.com>
2013-03-03 04:47:48 +0900

    KnuDormitoryFoodMenuModel 수정
    1. KnuDormitoryFoodMenuParser에서 가져온 WeekFoodMenu를 저장하고 있다가, 다음 입력이 왔을
    경우, 새로 웹에서 가져오지 않고 저장해 두었던 WeekFoodMenu를 사용하되, 다음 입력이 저장된 WeekFoodMenu에
    포함된 날이 아닌 경우 웹에서 새로운 데이터를 가져와 파싱하도록 변경 
    2. getWeekFoodMenu() 메소드 생성

--- src/kr/yudonguk/kangwonuniv/foodmenu/data/model/KnuDormitoryFoodMenuModel.java
+++ src/kr/yudonguk/kangwonuniv/foodmenu/data/model/KnuDormitoryFoodMenuModel.java
@@ -4,74 +4,85 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Calendar;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 import kr.yudonguk.kangwonuniv.foodmenu.data.FoodMenu;
 import kr.yudonguk.kangwonuniv.foodmenu.data.WeekFoodMenu;
 import kr.yudonguk.kangwonuniv.foodmenu.data.WeekFoodMenu.Week;
 import kr.yudonguk.kangwonuniv.foodmenu.data.parser.FoodMenuParser;
 import kr.yudonguk.kangwonuniv.foodmenu.data.parser.KnuDormitoryFoodMenuParser;
+import kr.yudonguk.util.calendar.CalendarUtil;
 
 public abstract class KnuDormitoryFoodMenuModel implements FoodMenuModel
 {
 	private static final FoodMenuParser parser = new KnuDormitoryFoodMenuParser();
-	private WeekFoodMenu weekFoodMenu = null;
-	private final Lock mWeekFoodMenuLock = new ReentrantLock();
+	private WeekFoodMenu mWeekFoodMenu = null;
+	private final Calendar mWeekFoodMenuCalendar = Calendar.getInstance();
 
 	@Override
 	public FoodMenu getData(int id)
 	{
-		if (weekFoodMenu == null)
-		{
-			synchronized (mWeekFoodMenuLock)
-			{
-				if (weekFoodMenu == null)
-				{
-					try
-					{
-						Calendar startDate = Calendar.getInstance();
-						startDate.clear();
-						startDate.add(Calendar.DAY_OF_MONTH, id);
-						int dayOfWeek = startDate.get(Calendar.DAY_OF_WEEK);
-
-						if (dayOfWeek == Calendar.SUNDAY)
-							startDate.add(Calendar.DAY_OF_MONTH, -6);
-						else
-							startDate.add(Calendar.DAY_OF_MONTH, -dayOfWeek
-									+ Calendar.MONDAY);
-
-						Calendar endDate = (Calendar) startDate.clone();
-						endDate.add(Calendar.DAY_OF_MONTH, 6);
-
-						String domitoryUrl = KnuDormitoryFoodMenuParser
-								.AddParameter(getKnuDormitoryUrl(), startDate,
-										endDate);
-						URL url = new URL(domitoryUrl);
-						weekFoodMenu = parser.parse(url);
-					}
-					catch (MalformedURLException e)
-					{
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					catch (IOException e)
-					{
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-					if (weekFoodMenu == null)
-						return null;
-				}
-			}
-		}
-
 		Calendar calendar = Calendar.getInstance();
 		calendar.clear();
 		calendar.add(Calendar.DAY_OF_MONTH, id);
+
+		WeekFoodMenu weekFoodMenu = null;
+
+		synchronized (mWeekFoodMenuCalendar)
+		{
+			if (mWeekFoodMenu != null)
+			{
+				Calendar startDate = Calendar.getInstance();
+				Calendar endDate = Calendar.getInstance();
+				CalendarUtil.getWeekBoundary(startDate, endDate,
+						mWeekFoodMenuCalendar, Calendar.MONDAY);
+
+				if (calendar.compareTo(startDate) >= 0
+						&& calendar.compareTo(endDate) <= 0)
+					weekFoodMenu = mWeekFoodMenu;
+			}
+
+			if (weekFoodMenu == null)
+			{
+				weekFoodMenu = getWeekFoodMenu(calendar);
+
+				if (weekFoodMenu == null)
+					return null;
+
+				mWeekFoodMenu = weekFoodMenu;
+				mWeekFoodMenuCalendar.setTime(calendar.getTime());
+			}
+		}
+
 		Week dayOfWeek = Week.toWeek(calendar.get(Calendar.DAY_OF_WEEK));
 		return weekFoodMenu.get(dayOfWeek);
+	}
+
+	public WeekFoodMenu getWeekFoodMenu(Calendar calendar)
+	{
+		WeekFoodMenu weekFoodMenu = null;
+		try
+		{
+			Calendar startDate = Calendar.getInstance();
+			Calendar endDate = Calendar.getInstance();
+
+			CalendarUtil.getWeekBoundary(startDate, endDate, calendar,
+					Calendar.MONDAY);
+
+			String domitoryUrl = KnuDormitoryFoodMenuParser.AddParameter(
+					getKnuDormitoryUrl(), startDate, endDate);
+			URL url = new URL(domitoryUrl);
+			weekFoodMenu = parser.parse(url);
+		} catch (MalformedURLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return weekFoodMenu;
 	}
 
 	public abstract String getKnuDormitoryUrl();
